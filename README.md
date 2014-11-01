9108217cd0cac638b79ddf82b059a24210e5f3ec
Jos Huybrighs <jhuybrighs@hotmail.com>
2014-11-01 11:55:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+##service.flatcheckbox.jquery
+
+###Description
+
+service.flatcheckbox.jquery is a small jQuery plugin that serves 2 purposes:
+
+- It styles a checkbox any way you want by hiding the `<input type="checbox" />` and  replacing it with a styled div. The css file that comes with the component creates a 'flat' checkbox (comparable with how you see checkboxes on Windows 8.x apps). 
+- It binds your event handler to the element.
+
+Although with CSS3 it is possible to re-style the default checkbox without using javascript I am not a fan of that solution because it still isn't supported across all browsers and, more importantly, you will anyhow need javascript to hook up an event handler.
+
+###Tutorial and Documentation
+
+You can find the tutorial and API description at [code.cwwonline.be](http://code.cwwonline.be/serviceflatcheckboxjquery).
---
+++ src/service.flatcheckbox.jquery.js
@@ -0,0 +1,88 @@
+/****************************************************************************************************** 
+ * A jquery plugin implementing a checkbox. 
+ * Usage:
+ *  - Element:
+ *      <input id="myCheckbox" type="checkbox" @( (Model.ShowPassedActivities != 0) ? "checked" : "") />
+
+ *      
+ *  - Instantiation:
+ *    1. With a label behind the checkbox
+ *      $('#checkbox').flatcheckbox({ label: 'Show passed activities',
+ *                                    onChecked: function () { ... },
+ *                                    onUnChecked: function () { ... }
+ *      });
+ *    2. Without a label
+ *      $('#checkbox').flatcheckbox({ onChecked: function () { ... },
+ *                                    onUnChecked: function ( { ... }
+ *      });
+ *
+ * version 1.0.0
+ *
+ * @requires jQuery 1.8.0 or later
+ *
+ * Copyright (c) Jos Huybrighs
+ * swcomponents.cwwonline.be
+ *
+ * Licensed under the MIT license.
+ * http://en.wikipedia.org/wiki/MIT_License
+ *
+ ******************************************************************************************************/
+; (function ($, window, document, undefined) {
+
+    var pluginName = "flatcheckbox";
+    var defaults = {
+        label: "",
+        onChecked: function () { },
+        onUnChecked: function () { }
+    };
+
+    function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this._checked = false;
+        this._init();
+    };
+
+    Plugin.prototype = {
+
+        _init: function () {
+            var self = this;
+            this._checked = $(this.element).prop('checked');
+            $(this.element).hide();
+            $(this.element).after('<div class="flcheckbox"><span class="ckbBox"></span><span class="ckbText">' + this.settings.label + '</span></div>');
+            var $flchkbox = $(this.element).next();
+            if (this._checked) {
+                $flchkbox.addClass('flchecked');
+            }
+            $flchkbox.bind('click', function () {
+                $flchkbox.toggleClass('flchecked');
+                self._checked = !self._checked;
+                $(self.element).prop('checked', self._checked);
+                if (self._checked) {
+                    self.settings.onChecked();
+                }
+                else {
+                    self.settings.onUnChecked();
+                }
+            });
+        }
+
+    };
+
+    $.fn[pluginName] = function (methodOrOptions) {
+        var instance = $(this).data(pluginName);
+        if (instance &&
+             methodOrOptions.indexOf('_') != 0) {
+            return instance[methodOrOptions](Array.prototype.slice.call(arguments, 1));
+        }
+        if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+            instance = new Plugin(this, methodOrOptions);
+            $(this).data(pluginName, instance);
+            return $(this);
+        }
+        $.error('Wrong call to ' + pluginName);
+    };
+})(jQuery, window, document);
+
